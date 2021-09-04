f6d6bd062fd3fb5ae147f1e7fe238879766378c3
Douglas Trajano <douglas.trajano@outlook.com>
2021-09-04 10:42:56 -0300

    olid_bucket

---
+++ terraform/variables.tf
@@ -0,0 +1,17 @@
+variable "name" {
+  description = "A name for the application."
+  type = string
+  default = "toxicity-detection"
+}
+
+variable "environment" {
+  description = "Environment. It will be part of the application name and a tag in AWS Tags."
+  type = string
+  default = "dev"
+}
+
+variable "region" {
+  description = "AWS Region name."
+  type = string
+  default = "us-east-1"
+}
