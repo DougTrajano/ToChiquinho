548b710fd5613b18b4b5b143f06690e0701fc285
Douglas Trajano <douglas.trajano@outlook.com>
2023-02-15 20:20:32 -0300

    Update unit tests

---
+++ tests/test_src_ml_environments.py
@@ -0,0 +1,5 @@
+from src.ml.environments import EnvironmentVariables
+
+def test_environment():
+    env = EnvironmentVariables()
+    assert type(env) == EnvironmentVariables
--- tests/test_src_ml_utils.py
+++ tests/test_src_ml_utils.py
@@ -1,7 +1,8 @@
 import os
 from src.ml.utils import (
     compute_pos_weight,
-    remaining_args_to_env
+    remaining_args_to_env,
+    flatten_dict
 )
 
 def test_compute_pos_weight():
@@ -13,3 +14,8 @@ def test_remaining_args_to_env():
     args = ["--aws_profile_name", "default"]
     remaining_args_to_env(args)
     assert os.environ["AWS_PROFILE_NAME"] == "default"
+
+def test_flatten_dict():
+    d = {"a": {"b": 1, "c": 2}, "d": 3}
+    flattened = flatten_dict(d)
+    assert flattened == {"a_b": 1, "a_c": 2, "d": 3}
