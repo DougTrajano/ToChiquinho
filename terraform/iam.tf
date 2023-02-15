db33bfcf9bb140722be0a6740ac12dce4f408143
Douglas Trajano <douglas.trajano@outlook.com>
2023-02-15 20:15:11 -0300

    Fix Security Hotspots

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,7 +1,7 @@
 resource "aws_iam_role" "iam_role" {
   name = "${local.name}-role"
 
-    # Change this to Fargate instead App Runner 
+  # Change this to Fargate instead App Runner 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -24,7 +24,7 @@ resource "aws_iam_role" "iam_role" {
 
   tags = merge(
     {
-        Name = "${local.name}-role"
+      Name = "${local.name}-role"
     },
     local.tags
   )
@@ -41,8 +41,7 @@ resource "aws_iam_role_policy" "bucket_policy" {
       {
         Effect = "Allow"
         Action = [
-          "s3:ListBucket",
-          "s3:HeadBucket",
+          "s3:ListBucket"
         ]
         Resource = concat(
           aws_s3_bucket.bucket.*.arn,
@@ -51,28 +50,40 @@ resource "aws_iam_role_policy" "bucket_policy" {
       {
         Effect = "Allow"
         Action = [
-          "s3:ListBucketMultipartUploads",
+          "s3:AbortMultipartUpload",
           "s3:GetBucketTagging",
-          "s3:GetObjectVersionTagging",
-          "s3:ReplicateTags",
-          "s3:PutObjectVersionTagging",
-          "s3:ListMultipartUploadParts",
-          "s3:PutObject",
           "s3:GetObject",
           "s3:GetObjectAcl",
-          "s3:GetObject",
-          "s3:AbortMultipartUpload",
-          "s3:PutBucketTagging",
-          "s3:GetObjectVersionAcl",
           "s3:GetObjectTagging",
-          "s3:PutObjectTagging",
           "s3:GetObjectVersion",
+          "s3:GetObjectVersionAcl",
+          "s3:GetObjectVersionTagging",
+          "s3:ListBucketMultipartUploads",
+          "s3:ListMultipartUploadParts",
+          "s3:PutBucketTagging",
+          "s3:PutObject",
+          "s3:PutObjectTagging",
+          "s3:PutObjectVersionTagging",
+          "s3:ReplicateTags",
         ]
         Resource = [
           for bucket in concat(aws_s3_bucket.bucket.*.arn) :
           "${bucket}/*"
         ]
       },
+      {
+        Sid    = "HTTPSOnly"
+        Effect = "Deny"
+        Action = "s3:*"
+        Resource = concat(
+          aws_s3_bucket.bucket.*.arn,
+        )
+        Condition = {
+          Bool = {
+            "aws:SecureTransport" = false
+          }
+        }
+      }
     ]
   })
 }
